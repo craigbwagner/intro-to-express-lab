1c56655c1b061904f241a5fe1a6c1407cf6a7491
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-05-29 16:14:14 -0400

    complete exercise 2, add validator, /roll/<number> rolls a random integer between 0 and the number parameter

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const validator = require('validator');
 const port = 3000;
 
 app.listen(port, () => {
@@ -9,4 +10,16 @@ app.listen(port, () => {
 app.get('/greetings/:username', (req, res) => {
 	const username = req.params.username;
 	res.send(`Hello there, ${username}`);
+});
+
+app.get('/roll/:diceSize', (req, res) => {
+	const dice = req.params.diceSize;
+
+	if (validator.isNumeric(dice)) {
+		diceNumber = Number(dice);
+		const roll = Math.floor(Math.random() * dice);
+		res.send(`You rolled a ${roll}`);
+	} else if (!validator.isNumeric(dice)) {
+		res.send('You must specify a number.');
+	}
 });
