cce81d934381df14fa414c174090388cd576e967
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-05-29 16:21:32 -0400

    complete exercise 3, respond with name of merchandise and price corresponding with index parameter

--- server.js
+++ server.js
@@ -23,3 +23,15 @@ app.get('/roll/:diceSize', (req, res) => {
 		res.send('You must specify a number.');
 	}
 });
+
+app.get('/collectibles/:index', (req, res) => {
+	const collectibles = [
+		{ name: 'shiny ball', price: 5.95 },
+		{ name: 'autographed picture of a dog', price: 10 },
+		{ name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 },
+	];
+	const index = req.params.index;
+	res.send(
+		`If you want to buy the ${collectibles[index].name}, it will cost you $${collectibles[index].price}`
+	);
+});
